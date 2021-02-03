40bd3dcb17e74c0b64c2e087560cf9a6b7995111
hizuka <1044909619@qq.com>
2021-02-03 12:01:39 +0800

    Delete dist directory

--- dist/wavesurfer.js
+++
@@ -1,9 +0,0 @@
-/*!
- * wavesurfer.js 4.3.0 (2021-01-20)
- * https://wavesurfer-js.org
- * @license BSD-3-Clause
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ })()
-;
